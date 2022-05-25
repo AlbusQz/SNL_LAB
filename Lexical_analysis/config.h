6e23b4cf1070728ba6c2b3595cff18f44a9ed943
LazyChildren <1204694006@qq.com>
2022-05-25 16:35:16 +0800

    Revert "部分词法分析 5.25"
    This reverts commit 95c997487645aa79dc4854182a5a6aa1937d64a0.

--- Lexical_analysis/config.h
+++ Lexical_analysis/config.h
@@ -1,13 +1,13 @@
 #pragma once
 #include<string>
 using namespace std;
-
+//const char sourcepath[50], tokenpath[50];
 
 
 
 enum Terminal
 {
-	IDENTIFIER,//标识符
+	ID,//标识符
 	CONST, //常量
 	//下面都是保留字
 	PROGRAM,
@@ -29,41 +29,15 @@ enum Terminal
 	READ,
 	WRITE,
 	//特殊符号
-	ADD,
-	MINUS,
-	MULTY,
-	DEVIDE,
-	LT,
 	ENDOFFILE,  //EOF
 	LEFTPAREN,
 	RIGHTPAREN,
 	LEFTMIDPAREN,
-	RIGHTMIDPAREN,
-	ERROR,
-	NONE,
-	EQUAL
+	RIGHTMIDPAREN
+
 };
 
-enum
-{
-	Program, ProgramHead, ProgramName, DeclarePart,
-	TypeDec, TypeDeclaration, TypeDecList, TypeDecMore,
-	TypeId, TypeName, BaseType, StructureType,
-	ArrayType, Low, Top, RecType,
-	FieldDecList, FieldDecMore, IdList, IdMore,
-	VarDec, VarDeclaration, VarDecList, VarDecMore,
-	VarIdList, VarIdMore, ProcDec, ProcDeclaration,
-	ProcDecMore, ProcName, ParamList, ParamDecList,
-	ParamMore, Param, FormList, FidMore,
-	ProcDecPart, ProcBody, ProgramBody, StmList,
-	StmMore, Stm, AssCall, AssignmentRest,
-	ConditionalStm, StmL, LoopStm, InputStm,
-	InVar, OutputStm, ReturnStm, CallStmRest,
-	ActParamList, ActParamMore, RelExp, OtherRelE,
-	Exp, OtherTerm, Term, OtherFactor,
-	Factor, Variable, VariMore, FieldVar,
-	FieldVarMore, CmpOp, AddOp, MultOp
-}; // 非终极符
+
 
 class Word
 {
@@ -73,27 +47,9 @@ public:
 };
 
 
-enum State
-{
-	START,
-	INID,
-	STATEEND,
-	DONE1,
-	DONE
-};
 
-class Identifier
-{
-public:
-	int index;
-	string text;
-	Identifier(int in,string cont)
-	{
-		index = in;
-		text = cont;
-	}
-	Identifier(){}
-};
+
+
 
 
 
@@ -101,30 +57,10 @@ class Token {
 public:
 	int line;
 	Terminal type;
-	int index; 
-	Token* next;
-	Token(int lin,Terminal typ,int in)
-	{
-		line = lin;
-		type = typ;
-		index = in;
-		next = NULL;
-	}
-
-	Token()
-	{
-		
-		next = NULL;
-	}
+	int index = -1; 
 };// token
 
 
 char getNextChar();
 char ungetNextChar();
 bool isReserved(string s);
-bool isLetter(char ch);
-bool isDigit(char ch);
-bool isSpe(char ch);
-void tokenPrint(Token* head);
-Terminal findNum(string word);
-Terminal findNum(char ch);
