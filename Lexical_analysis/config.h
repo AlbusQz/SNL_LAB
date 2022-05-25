44fbeabf28e1c8c0928a2a9a087afda08c3bf1bd
LazyChildren <1204694006@qq.com>
2022-05-25 16:36:52 +0800

    Revert "Revert "部分词法分析 5.25""
    This reverts commit 6e23b4cf1070728ba6c2b3595cff18f44a9ed943.

--- Lexical_analysis/config.h
+++ Lexical_analysis/config.h
@@ -1,13 +1,13 @@
 #pragma once
 #include<string>
 using namespace std;
-//const char sourcepath[50], tokenpath[50];
+
 
 
 
 enum Terminal
 {
-	ID,//标识符
+	IDENTIFIER,//标识符
 	CONST, //常量
 	//下面都是保留字
 	PROGRAM,
@@ -29,15 +29,41 @@ enum Terminal
 	READ,
 	WRITE,
 	//特殊符号
+	ADD,
+	MINUS,
+	MULTY,
+	DEVIDE,
+	LT,
 	ENDOFFILE,  //EOF
 	LEFTPAREN,
 	RIGHTPAREN,
 	LEFTMIDPAREN,
-	RIGHTMIDPAREN
-
+	RIGHTMIDPAREN,
+	ERROR,
+	NONE,
+	EQUAL
 };
 
-
+enum
+{
+	Program, ProgramHead, ProgramName, DeclarePart,
+	TypeDec, TypeDeclaration, TypeDecList, TypeDecMore,
+	TypeId, TypeName, BaseType, StructureType,
+	ArrayType, Low, Top, RecType,
+	FieldDecList, FieldDecMore, IdList, IdMore,
+	VarDec, VarDeclaration, VarDecList, VarDecMore,
+	VarIdList, VarIdMore, ProcDec, ProcDeclaration,
+	ProcDecMore, ProcName, ParamList, ParamDecList,
+	ParamMore, Param, FormList, FidMore,
+	ProcDecPart, ProcBody, ProgramBody, StmList,
+	StmMore, Stm, AssCall, AssignmentRest,
+	ConditionalStm, StmL, LoopStm, InputStm,
+	InVar, OutputStm, ReturnStm, CallStmRest,
+	ActParamList, ActParamMore, RelExp, OtherRelE,
+	Exp, OtherTerm, Term, OtherFactor,
+	Factor, Variable, VariMore, FieldVar,
+	FieldVarMore, CmpOp, AddOp, MultOp
+}; // 非终极符
 
 class Word
 {
@@ -47,9 +73,27 @@ public:
 };
 
 
+enum State
+{
+	START,
+	INID,
+	STATEEND,
+	DONE1,
+	DONE
+};
 
-
-
+class Identifier
+{
+public:
+	int index;
+	string text;
+	Identifier(int in,string cont)
+	{
+		index = in;
+		text = cont;
+	}
+	Identifier(){}
+};
 
 
 
@@ -57,10 +101,30 @@ class Token {
 public:
 	int line;
 	Terminal type;
-	int index = -1; 
+	int index; 
+	Token* next;
+	Token(int lin,Terminal typ,int in)
+	{
+		line = lin;
+		type = typ;
+		index = in;
+		next = NULL;
+	}
+
+	Token()
+	{
+		
+		next = NULL;
+	}
 };// token
 
 
 char getNextChar();
 char ungetNextChar();
 bool isReserved(string s);
+bool isLetter(char ch);
+bool isDigit(char ch);
+bool isSpe(char ch);
+void tokenPrint(Token* head);
+Terminal findNum(string word);
+Terminal findNum(char ch);
--- Lexical_analysis/util.cpp
+++ Lexical_analysis/util.cpp
@@ -1,17 +1,79 @@
 ﻿#include "config.h"
 #include <iostream>
 #include <string>
+
+Token* token_head = new Token();
+Token* token_tail = token_head;
+Identifier identifier_list[500];
+const char sourcepath[50] = "source_code.txt",tokenpath[50]="token_list.txt";
+int id_list_tail = 0;
 int main(void)
 {
 	int lineNum = 1;
 	int index = 0;
-	char ch = getNextChar();
+	char ch=' ';
+	State state = START;
 	string cur_word = "";
-	while (ch != ENDOFFILE)
+	while (ch != '?')
 	{
+		ch = getchar();
+		if (ch == '\n')
+			lineNum++;
+		if (state == DONE)
+		{
+			cur_word = "";
+			state = START;
+		}
+			
+		switch (state)
+		{
+		case START:
+			if (isLetter(ch))
+			{
+				state = INID;
+				cur_word += ch;
+			}
+			if (isSpe(ch))
+			{
+				token_tail->next = new Token(lineNum, findNum(ch), -1);
+				token_tail = token_tail->next;
+				state = DONE;
+			}
+			break;
+		case INID:
+			if (isLetter(ch) || isDigit(ch))
+			{
+				cur_word += ch;
+			}
+			else
+			{
+				state = DONE;
+				if (isReserved(cur_word))
+				{
+					token_tail->next = new Token(lineNum, findNum(cur_word), -1);
+					token_tail = token_tail->next;
+				}
+				else
+				{
+					identifier_list[id_list_tail] = Identifier(id_list_tail, cur_word);
+					id_list_tail++;
+					token_tail->next = new Token(lineNum,IDENTIFIER,id_list_tail-1);
+					token_tail = token_tail->next;
+				}
+				cur_word = "";
+			}
+			break;
+		case STATEEND:
+			break;
+		case DONE:
+
+			break;
+		default:
+			break;
+		}
 		
 	}
-
+	tokenPrint(token_head->next);
 
 
 	return 0;
